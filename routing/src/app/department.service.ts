cdec16e9f5d6bf4329a4da9964a6d18526064258
Miriam BARATTONI <baramir@nube.local>
2019-03-07 14:20:54 +0100

    20190307

---
+++ routing/src/app/department.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { IDepartment } from './department';
+import { throwError, Observable } from 'rxjs';
+import {catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DepartmentService {
+	private _url: string = "/assets/data/departments.json";
+
+  constructor(private _http: HttpClient) { }
+	
+	getDepartments(): Observable<IDepartment[]>{
+		return this._http.get<IDepartment[]>(this._url)
+				.pipe(catchError(this.errorHandler));
+	}
+	
+	errorHandler(error: HttpErrorResponse){
+		return throwError(error.message || "Server Error!");
+	}
+	
+}
